332a246e3755ee01d4ec8581105720619294d864
Michael Ferguson <mickferg1@gmail.com>
2014-06-08 09:53:44 +1000

    Auto Commit - Sun Jun  8 09:53:32 AUSEST 2014 (Fergoman007-PC)

--- src/main/java/fergoman123/mods/fergotools/util/UtilResourceLocation.java
+++ src/main/java/fergoman123/mods/fergotools/util/UtilResourceLocation.java
@@ -1,14 +1,17 @@
 package fergoman123.mods.fergotools.util;
 
+import fergoman123.mods.fergotools.lib.Reference;
 import net.minecraft.util.ResourceLocation;
 
-/**
- * Created by Fergoman123 on 03/05/2014.
- */
 public class UtilResourceLocation {
 
     public static ResourceLocation getResourceLocation(String modid, String path)
     {
         return new ResourceLocation(modid, path);
+    }
+
+    public static ResourceLocation getResourceLocation(String path)
+    {
+        return getResourceLocation(Reference.textureLocGui, path);
     }
 }
